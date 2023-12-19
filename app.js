49329efae4aeff54ba0c621322ab492013260ece
Tarun Malempati <24860718+tarunmalempati@users.noreply.github.com>
2023-12-19 19:11:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware to parse JSON data
 app.use(express.json());
