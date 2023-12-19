6948682445e0e312715370b76fd842381c71f943
root <root@ip-172-31-9-168.ap-south-1.compute.internal>
2023-12-19 13:07:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware to parse JSON data
+app.use(express.json());
+
+// POST request handler
+app.post('/hello', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
